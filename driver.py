7fd88d2126e7666421ca8e4fe29c36e4e89e3b44
zetaro123 <33451360+zetaro123@users.noreply.github.com>
2018-09-02 17:36:30 +0530

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,35 +1,131 @@
-import logging
-import os
-
-#Reads and returns the list of files from a directory
-def read_directory(mypath):
-    current_list_of_files = []
-
-    while True:
-        for (_, _, filenames) in os.walk(mypath):
-            current_list_of_files = filenames
-        logging.info("Reading the directory for the list of file names")
-        return current_list_of_files
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
 
 
-# Function you will be working with
-def create_knowledge_graph(contents_of_input_file, name_of_input_file):
-    # Through this function you have to use the contents of each file to create a knowledge graph
-    # The output has to be saved in the data/output folder with the same name as data/input file
-    # Note the writing to file has to be handled by you.
-    pass
 
-#Main function
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+def get_subject_from_sentence(sentence):
+	words = word_tokenize(str(sentence))
+	string = ""
+	for i in range (0,len(words)):
+		list1 = nltk.pos_tag(nltk.word_tokenize(words[i]))
+		if(list1[0][1] != 'VBZ' and list1[0][1] != 'NNS' ):
+			string = string + words[i] + " "
+		else:
+			break
+	return string
 
-    #Folder where the input files are present
-    mypath = "data//input"
-    list_of_input_files = read_directory(mypath)
-    logging.debug(list_of_input_files)
-    for each_file in list_of_input_files:
-        with open(os.path.join(mypath,each_file), "r") as f:
-            file_contents = f.read()
 
-            create_knowledge_graph(file_contents, each_file)
-            # end of code
+
+def get_object_from_sentence(sentence):
+	words = word_tokenize(str(sentence))
+	length = len(words)
+	if length != 0:
+		return words[length-2]
+	else:
+		return ""
+
+
+def  get_verb_from_sentence(sentence):
+	words = word_tokenize(str(sentence))
+	string = ""
+	for term in words:
+		list1 = nltk.pos_tag(nltk.word_tokenize(term))
+		if(list1[0][1] == 'VBZ' or list1[0][1] == 'NNS' or list1[0][1] == 'IN'):
+			string = string + term + " "
+	return string
+
+
+
+def get_subject_from_sentences(list_of_sentences):
+	list_subject = []
+	for sentence in list_of_sentences:
+		words = word_tokenize(str(sentence))
+		string = ""
+		for i in range (0,len(words)):
+			list1 = nltk.pos_tag(nltk.word_tokenize(words[i]))
+			if(list1[0][1] != 'VBZ' and list1[0][1] != 'NNS' ):
+				string = string + words[i] + " "
+			else:
+				break;
+		list_subject.append(string)
+				
+
+	return list_subject
+
+
+def get_object_from_sentences(list_of_sentences):
+	list_object = []
+	#list_subject = get_subject_from_sentences(list_of_sentences)
+	for sentence in list_of_sentences:
+		words = word_tokenize(str(sentence))
+		length = len(words)
+		if length != 0:
+			if words[length-2] not in list_object:
+				list_object.append(words[length-2])
+	return list_object
+
+
+
+def get_verb_part_from_sentences(list_of_sentences):
+	list_subject = get_subject_from_sentences(list_of_sentences)
+	list_object = get_object_from_sentences(list_of_sentences)
+	list_verbs = []
+	for sentence in list_of_sentences:
+		string = ""
+		words = word_tokenize(str(sentence))
+		for term in words:
+			list1 = nltk.pos_tag(nltk.word_tokenize(term))
+			if(list1[0][1] == 'VBZ' or list1[0][1] == 'NNS' or list1[0][1] == 'IN'):
+				string = string + term + " "
+		if(string not in list_verbs):
+			list_verbs.append(string)
+	return list_verbs
+
+
+
+def different_conclusions_from_sentences(list_sentences, list_subject, list_object, list_verb):
+	my_file = open(input_file,'w')
+	answer = []
+	for sentence in list_sentences:
+		sub = get_subject_from_sentence(sentence)
+		ver = get_verb_from_sentence(sentence)
+		obj = get_object_from_sentence(sentence)
+		answer.append([sub,ver,obj])
+
+	for i in range(0,len(answer)):
+		for j in range(0, len(answer)):
+			if(answer[i][1] == answer[j][1] and i != j):
+				a = [answer[j][0], answer[i][1], answer[i][2]]
+				if(a not in answer):
+					answer.append(a)
+
+	for lst in answer:
+		my_file.write(str(lst[0])+","+str(lst[1])+","+str(lst[2])+"\n")
+
+	my_file.close()
+
+
+
+input_sentences = []
+subject_list = []
+object_list = []
+verb_list = []
+
+print("Enter name of input text file: ")
+input_file = str(input())
+
+with open(input_file,'r') as my_file:
+	input_sentences = [line.strip() for line in my_file]
+
+
+subject_list = get_subject_from_sentences(input_sentences)
+object_list = get_object_from_sentences(input_sentences)
+verb_list = get_verb_part_from_sentences(input_sentences)
+
+# print(subject_list)
+# print(object_list)
+# print(verb_list)
+different_conclusions_from_sentences(input_sentences, subject_list, object_list, verb_list)
+
